f808d42138923ab832ebae97dbee0cde3dc43abe
kajalkushwaha2310 <kajalkushwaha231096@gmail.com>
2022-12-07 16:44:37 +0530

    why choose us page complete

---
+++ src/components/components/ChooseUs_Card_Detail.js
@@ -0,0 +1,36 @@
+import GroupsIcon from '@mui/icons-material/Groups';
+
+export const ChooseUs_Card_Detail = [
+    {   
+        icon:<GroupsIcon style={{color:"black", fontSize:"4.1rem" ,marginTop:".5rem"}}/>,
+        title:"Well Balanced Team",
+        para:"With a deep understanding and experience of the latest technologies, we have delivered over 50 successful projects.",
+    },
+    {
+        icon:<i class="fa-solid fa-people-group" style={{color:"black", fontSize:"4.1rem", marginTop:".5rem"}}></i> ,
+        title:"Strong Expertise",
+        para:"With our core field of expertise being Android, Blockchain, iOS, React, etc. our development experts remain on the cutting edge of the newest and most successful technology trends and popular applications on the market.",
+    },
+    {
+        icon:<i class="fa-solid fa-person-rays" style={{color:"black", fontSize:"4.1rem", marginTop:".5rem"}}></i>,
+        title:"Customer Centric Approach",
+        para:"We ensure that the customer Idea is at the center of a business's philosophy. We always work on end-user experience.",
+    },
+    {
+        icon:<i class="fa-solid fa-gears" style={{color:"black", fontSize:"4.1rem",marginTop:".5rem"}}></i>,
+        title:"Advanced Technology",
+        para:"Our in-house IT and development team allows us to provide advance technology-enabled solutions that can make processes more efficient and effective.",
+    },
+    {
+        icon:<i class="fa-solid fa-hand-holding-dollar" style={{color:"black", fontSize:"4.1rem",marginTop:".5rem"}}></i>,
+        title:"Low Investment Cost",
+        para:"Low cost doesn’t necessarily mean poor quality or poor service. By diving into our professional workforce expert, Direct Sourcing Solutions can provide world-class services at the most competitive prices.",
+    },
+    {
+        icon:<i class="fa-solid fa-person-dress-burst" style={{color:"black", fontSize:"4.1rem", marginTop:".5rem"}}></i>,
+        title:"Domain Expertise",
+        para:"With multiple projects done on Travel, Healthcare, Ecommerce, Education, and more. We know the trends well and having years of expertise in different business domains.",
+    },
+]
+
+
--- src/components/components/Why_Choose_Us.js
+++ src/components/components/Why_Choose_Us.js
@@ -1,59 +1,85 @@
-import React from 'react'
-import styled from 'styled-components';
-import CurrencyBitcoinIcon from "@mui/icons-material/CurrencyBitcoin";
+import React, { useState } from "react";
+import styled from "styled-components";
+import { ChooseUs_Card_Detail } from "./ChooseUs_Card_Detail";
 
 const Why_Choose_Us = () => {
+  const [state, setState] = useState(ChooseUs_Card_Detail);
   return (
-   <>
-    <Wrapper>
-    <div class="container">
-    <div class="row">
-        <div class="col-6 mt-3">
-            <div class="card">
-                <div class="card-horizontal">
-                <div className="card_icon">
-                      <CurrencyBitcoinIcon className="icon"/>
-                    </div>
-                    
-             
-                    <div class="card-body">
-                        <h4 class="card-title">Card title</h4>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
-                </div>
-               
-            </div>
+    <>
+      <Wrapper>
+        <div class="container">
+        <div className="content">
+        <h1>Why Choose Us</h1>
+        <p>Here are the reasons you can depend on Ftechiz Solutions to keep you and your business up and running:</p>
         </div>
-    </div>
-</div>
-    </Wrapper>
-   </>
-  )
-}
+          <div class="grid grid_two_column">
+            {state.map((currentData) => {
+              const { icon, title, para } = currentData;
+              return (
+                <>
+                  <div class="card">
+                    <div class=" grid-two-column">
+                      <div className="card_icon">
+                        <div className="icon">{icon}</div>
+                      </div>
+                      <div class="card-body">
+                        <h3 class="card-title">{title}</h3>
+                        <p class="card-text" style={{ textAlign: "justify" }}>
+                          {para}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </>
+              );
+            })}
+          </div>
+        </div>
+      </Wrapper>
+    </>
+  );
+};
 const Wrapper = styled.section`
-.card-horizontal {
-    display: flex;
-    flex: 1 1 auto;
-      border:1px solid red;
-    .card{
+  padding: 3rem 0;
+  background: ${({ theme }) => theme.colors.bg};
+  .grid {
+    display: grid;
+    gap: 3rem;
+    margin: 8rem 2rem;
+  }
+  .content{
+    margin-top:3rem;
+    text-align:center;
+    p{
+      font-size:1.6rem; 
     }
-.card_icon {
-      width: 10rem;
-      height: 10rem;
-      background: ${({ theme }) => theme.colors.hover_color};
-      border-radius: 50%;
-      display: inline-block;
-      padding: 2rem 0rem;
-      margin: auto;
-      margin-bottom: 2rem;
-      .icon {
-        font-size: 5rem;
-        color: ${({ theme }) => theme.colors.color_white};
+  }
+  .card {
+    width: 58rem;
+    display: flex;
+    flex-direction: row;
+    background: ${({ theme }) => theme.colors.card_bg};
+    padding: 1rem 1.5rem;
+    border-radius: 1rem;
+    h3 {
+      margin: 1rem 0rem;
+    }
+    .grid-two-column {
+      display: flex;
+      flex-direction: row;
+      gap: 6rem;
+      grid-template-columns: 0.5fr 1fr;
+      .card_icon {
+        width: 10rem;
+        height: 10rem;
+        background: ${({ theme }) => theme.colors.helper};
+        border-radius: 50%;
+        display: inline-block;
+        padding: 3rem 2.8rem;
+        margin: auto;
+        margin-bottom: 8rem;
       }
     }
-    .card-text{
-      color:red;
-    }
-}
+  }
 `;
-export default Why_Choose_Us
+export default Why_Choose_Us;
