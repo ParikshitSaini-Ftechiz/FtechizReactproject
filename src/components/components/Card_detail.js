c90174bc73edecac2fe5e5b9e48f070df0ad84ed
kajalkushwaha2310 <kajalkushwaha231096@gmail.com>
2022-12-07 13:27:20 +0530

    changed icons css

--- src/components/components/Card_detail.js
+++ src/components/components/Card_detail.js
@@ -7,31 +7,31 @@ import BackupOutlinedIcon from '@mui/icons-material/BackupOutlined';
 
 export const Card_detail = [
     {   
-        icon:<svg xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="M19.25 40.85V36.6h-6.3v-2.2h4.3V13.6h-4.3v-2.25h6.3v-4.2h2.15v4.25h5.25V7.15h2.2v4.35q2.7.3 4.35 2.225 1.65 1.925 1.65 4.525 0 1.5-.6 2.825-.6 1.325-1.6 2.325 2 .8 3.1 2.525 1.1 1.725 1.1 3.825 0 2.9-1.975 4.875T30.05 36.6h-1.2v4.25h-2.2V36.6H21.4v4.25Zm.15-17.95h8.65q1.95 0 3.275-1.35t1.325-3.3q0-2-1.325-3.325Q30 13.6 28.05 13.6H19.4Zm0 11.5h10.65q1.95 0 3.275-1.35t1.325-3.3q0-2-1.325-3.325Q32 25.1 30.05 25.1H19.4Z"></path></svg>,
+        icon:<CurrencyBitcoinIcon style={{color:"white", fontSize:"5rem", marginTop:".5rem"}}/>,
         title:"Blockchain Development Company",
         para:"Being a leading Mobile App Development Company, Ftechiz develops the best user-friendly mobile applications for iOS and Android users both. Our Talented Mobile app developers have the ability to build custom innovative solutions that are designed and delivered to your specific requirement.",
         btn_text:"Read Me",
     },
     {
-        icon:<PhoneAndroidIcon />,
+        icon:<PhoneAndroidIcon  style={{color:"white", fontSize:"5rem", marginTop:".5rem"}}/>,
         title:"Mobile App Development Company",
         para:"Being a leading Mobile App Development Company, Ftechiz develops the best user-friendly mobile applications for iOS and Android users both. Our Talented Mobile app developers have the ability to build custom innovative solutions that are designed and delivered to your specific requirement.",
         btn_text:"Read Me",
     },
     {
-        icon:<DevicesIcon/>,
+        icon:<DevicesIcon style={{color:"white", fontSize:"5rem", marginTop:".5rem"}}/>,
         title:"Website Development Company",
         para:"Ftechiz is a leading web development company offering all kinds of custom built websites, web portals and web applications for all types of business verticals. We use latest cutting edge technologies, advanced frameworks and proven methodologies to ensure that our solutions are future-ready and scalable. ",
         btn_text:"Read Me",
     },
     {
-        icon:<ComputerIcon/>,
+        icon:<ComputerIcon style={{color:"white", fontSize:"5rem", marginTop:".5rem"}}/>,
         title:"Website Designing Company",
         para:"Being a Top Website Designing Company, Ftechiz bring together deep industry expertise and the latest IT advancements to deliver custom solutions and products that perfectly fit the needs and behaviour of their users. We use latest cutting-edge technologies, tools, advanced frameworks, and proven strategies to make sure that our solutions are future-ready and conformable. We have a pool of skilled designers who creates attractive designs for retail, banking, manufacturing, healthcare, education and other industries.",
         btn_text:"Read Me",
     },
     {
-        icon:<GroupsIcon />,
+        icon:<GroupsIcon  style={{color:"white", fontSize:"5rem", marginTop:".5rem"}}/>,
         title:"Website App Development Company",
         para:"Ftechiz believes in providing web application development services with valued consulting and analysis solutions. Through our top-notch web app development services, we became the partner of growing businesses and industry leaders in their success journey. Our team specializes in web app development services with robust technology, latest frameworks and tools, and are capable of picking the project from scratch to delivery. We have hands-on experience in building world-class B2B and B2C web solutions.",
         btn_text:"Read Me",
