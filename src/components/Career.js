20192b0547fe95c636bac674b9cb190b59aedaac
KajalKushwahaDeveloper <kajalkushwaha231096@gmail.com>
2022-12-23 20:45:18 +0530

    career_contact component has been created

--- src/components/Career.js
+++ src/components/Career.js
@@ -1,25 +1,99 @@
-import React from 'react'
-import Main_Section from './components/components/Main_Section'
+import React from "react";
+import Main_Section from "./components/components/Main_Section";
+import Para_Heading from "./components/components/DropDown_components.js/Para_Heading";
+import styled from "styled-components";
+import Career_Contact from "./components/Career_component.js/Career_Contact";
 
 const Career = () => {
   const data = {
-    nameData : "We Seek Innovators, Disrupters and Dreamers",
+    nameData: "We Seek Innovators, Disrupters and Dreamers",
   };
   const para = {
-    paraData : "Are You Smart, Creative and Driven? We're hiring... Explore a World of Big Opportunities",
+    paraData:
+      "Are You Smart, Creative and Driven? We're hiring... Explore a World of Big Opportunities",
   };
   const image = {
-    imageData : "./images/career.jpg",
-  }
+    imageData: "./images/career.jpg",
+  };
   const btn = {
-    btnData : "Let's talk",
-  }
+    btnData: "Let's talk",
+  };
+
+  const careerHeading = {
+    headingData: "Why Work with Ftechiz?",
+  };
+  const careerPara = {
+    paraData:
+      "Ftechiz promotes a culture of impacting users’ lives with our innovations. We aspire to build a diverse and inclusive culture, where everyone feels respected and our people are empowered to expand their thinking, creativity and experience to realize their long-term career goals. We are looking for talents who share, understand our vision and ready to contribute to it. Our company provides an environment with open communication, togetherness and equal opportunities.",
+    paraData2:
+      "We’re passionate about constantly increasing and forever keen to find dynamic talent in our team. Join us to level up not just the company’s strength but your own as well.",
+  };
   return (
     <>
       {/* {myName} */}
-      <Main_Section myData={data} para={para} image={image} btn={btn}/>
+      <Main_Section myData={data} para={para} image={image} btn={btn} />
+      <Para_Heading para1={careerPara} heading={careerHeading} />
+      <Wrapper>
+        <div className="container">
+          <div className="main_div">
+            <h1>Join the Ftechiz Team!</h1>
+            <p>
+              We always keep an eye out for talented people to join our team.
+              Even if we don’t have any opening positions at the moment, just
+              Drop Your Resume and Details.
+            </p>
+          </div>
+        </div>
+      </Wrapper>
+
+      <Section>
+<div className="container">
+<div className="grid grid-two-column">
+  <div>
+    <h2>Just Drop Your Resume And Details</h2>
+    <Career_Contact/>
+  </div>
+  <div></div>
+</div>
+</div>
+      </Section>
     </>
   );
+};
+const Wrapper = styled.section`
+  padding: 9rem 0;
+  .main_div {
+    background: ${({ theme }) => theme.colors.component_bg};
+    padding: 7rem;
+    border-radius: 2rem;
+    h1 {
+      color: ${({ theme }) => theme.colors.color_white};
+      text-align: center;
+      margin-bottom: 2rem;
+    }
+    p {
+      color: ${({ theme }) => theme.colors.color_white};
+    }
+  }
+
+`;
+// contact page//
+
+const Section = styled.section `
+.grid {
+  display: grid;
+  gap: 9rem;
+  margin:8rem 16rem 0rem 16rem;
 }
 
-export default Career
+.grid_two_column {
+  grid-template-columns: 1fr 1fr;
+
+}
+h2{
+  color: ${({ theme }) => theme.colors.heading};
+      text-align: center;
+}
+`;
+
+export default Career;
---
+++ src/components/components/Career_component.js/Career_Contact.js
@@ -0,0 +1,211 @@
+import React from "react";
+import "../../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { NavLink } from "react-router-dom";
+import { Button } from "../components/Button";
+import styled from "styled-components";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+
+import * as Yup from "yup";
+
+const Career_Contact = () => {
+  const defaultValues = {
+    name: "",
+    email: "",
+    number: "",
+    technolgy: "",
+    country: "",
+    id: "",
+    desc: "",
+  };
+  const validationSchema = Yup.object().shape({
+    name: Yup.string().min(2).max(24).required("Please enter your Name"),
+    email: Yup.string().email().required("please enter your Email"),
+    number: Yup.string().min(10).max(12).required("Please enter your Contact Number"),
+    technolgy: Yup.string().required("Please enter your technolgy"),
+    country: Yup.string().min(2).max(24).required("Please enter your Country"),
+    id: Yup.string()
+      .min(1)
+      .max(25)
+      .required("Please enter your Skype ID/Whatsapp No"),
+   desc: Yup.string()
+      .min(1)
+      .required("Required"),
+  });
+
+  const handleSubmit = (values) => {
+    console.log("values", values);
+  };
+
+  return (
+    <>
+      <Wrapper>
+        <div className="container contact_form">
+          <Formik
+            initialValues={defaultValues}
+            validationSchema={validationSchema}
+            onSubmit={handleSubmit}
+          >
+            <Form className="row g-3 ">
+              <div className="col-md-12">
+                <div className="form-outline">
+                  <label for="validationCustom01" className="form-label">
+                    Name
+                  </label>
+                  <Field
+                    type="name"
+                    className="form-control"
+                    id="validationCustom01"
+                    name="name"
+                  />
+                  <p>
+                    <ErrorMessage name="name" />
+                  </p>
+                </div>
+              </div>
+              <div className="col-md-12">
+                <div className="form-outline">
+                  <label for="validationCustom02" className="form-label">
+                    Email Address
+                  </label>
+                  <Field
+                    type="email"
+                    className="form-control"
+                    id="validationCustom02"
+                    name="email"
+                  />
+                  <p>
+                    <ErrorMessage name="email" />
+                  </p>
+                </div>
+              </div>
+              <div className="col-md-12">
+                <div className="form-outline">
+                  <label for="validationCustom01" className="form-label">
+                    Contact Number
+                  </label>
+                  <Field
+                    type="number"
+                    className="form-control"
+                    id="validationCustom01"
+                    name="number"
+                  />
+                  <p>
+                    <ErrorMessage name="number" />
+                  </p>
+                </div>
+              </div>
+              <div className="col-md-12">
+                <div className="form-outline">
+                  <label for="validationCustom02" className="form-label">
+                    technolgy
+                  </label>
+                  <Field
+                    type="text"
+                    className="form-control"
+                    id="validationCustom02"
+                    name="technolgy"
+                  />
+                  <p>
+                    <ErrorMessage name="technolgy" />
+                  </p>
+                </div>
+              </div>
+              <div className="col-md-12">
+                <div className="form-outline">
+                  <label for="validationCustom02" className="form-label">
+                    Country
+                  </label>
+                  <Field
+                    type="text"
+                    className="form-control"
+                    id="validationCustom02"
+                    name="country"
+                  />
+                  <p>
+                    <ErrorMessage name="country" />
+                  </p>
+                </div>
+              </div>
+              <div className="col-md-12">
+                <div className="form-outline">
+                  <label for="validationCustom02" className="form-label">
+                    (Skype ID/Whatsapp No)
+                  </label>
+                  <Field
+                    type="number"
+                    className="form-control"
+                    id="validationCustom02"
+                    name="id"
+                  />
+                  <p>
+                    <ErrorMessage name="id" />
+                  </p>
+                </div>
+              </div>
+     
+              <div className="col-md-12">
+               <div className="form-outline">
+                 <label for="validationCustom02" className="form-label">
+                   Project Description
+                 </label>
+                 <Field as="textarea"
+                   type="text"
+                   class="form-control"
+                   rows="20"
+                   column="10"
+                   name="desc"
+                   id="desc"
+                 />
+                 <p><ErrorMessage name="desc"/></p>
+               </div>
+               </div>
+              <div className="col-12">
+                <NavLink to="/">
+                  <Button
+                    type="submit"
+                    className="submit_btn"
+                  >
+                    Submit
+                  </Button>
+                </NavLink>
+              </div>
+            </Form>
+          </Formik>
+        </div>
+      </Wrapper>
+    </>
+  );
+};
+const Wrapper = styled.div`
+  padding: 5rem 0;
+  color: ${({ theme }) => theme.colors.color_white};
+  font-size: 1.7rem;
+  .container {
+    padding: 4rem;
+  }
+  .contact_form {
+    background: ${({theme}) => theme.colors.bg};
+     p{
+      color:red;
+      font-size: 1.9rem;
+     }
+    input,
+    textarea {
+      width: 100%;
+      height: 5rem;
+      font-size: 1.5rem;
+    }
+
+    .submit_btn {
+      background: ${({ theme }) => theme.colors.btn_color};
+      display: block;
+      margin: auto;
+      margin-top: 2rem;
+      padding: 1rem 6rem;
+      color: ${({ theme }) => theme.colors.color_white};
+      font-weight: 600;
+      font-size: 1.5rem;
+    }
+  }
+`;
+export default Career_Contact;
