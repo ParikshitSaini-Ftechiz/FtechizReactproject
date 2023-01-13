9dcdf21e854b54c61b7d1322c4a303d4eaf0d74c
KajalKushwahaDeveloper <kajalkushwaha231096@gmail.com>
2023-01-13 13:39:16 +0530

    Added active class

--- src/components/components/Tool_Tech.js
+++ src/components/components/Tool_Tech.js
@@ -3,8 +3,13 @@ import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 const Tool_Tech = ({ tool_tech_para, tool_tech_heading }) => {
+  const [active, setActive] = useState("");
   const { paraData } = tool_tech_para;
   const { headingData } = tool_tech_heading;
+
+  const handleClick = (event) => {
+    setActive(event.target.id);
+  };
   return (
     <>
       <Wrapper>
@@ -12,24 +17,66 @@ const Tool_Tech = ({ tool_tech_para, tool_tech_heading }) => {
           <h1>{headingData}</h1>
           <p>{paraData}</p>
           <div className="grid grid-five-column">
-            <div className="first_column active">
-              <NavLink to="" activeClassName="active-link">
-                <div className="first_column_text">Frontend</div>
+            <div className="first_column">
+            <NavLink to="" className="first_column_text">
+                <div
+                  key={1}
+                  className={active === "1" ? "active " : undefined}
+                  id={"1"}
+                  onClick={handleClick}
+                >
+                  Frontend
+                </div>
               </NavLink>
-              <NavLink to="" activeClassName="active-link">
-                <div className="first_column_text">Backend</div>
+              <NavLink to="" className="first_column_text">
+                <div
+                  key={2}
+                  className={active === "2" ? "active" : undefined}
+                  id={"2"}
+                  onClick={handleClick}
+                >
+                  Backend
+                </div>
               </NavLink>
-              <NavLink to="" activeClassName="active-link">
-                <div className="first_column_text">Mobile App Development</div>
+              <NavLink to="" className="first_column_text">
+                <div
+                  key={3}
+                  className={active === "3" ? "active" : undefined}
+                  id={"3"}
+                  onClick={handleClick}
+                >
+                  Mobile App Development
+                </div>
               </NavLink>
-              <NavLink to="" activeClassName="active-link">
-                <div className="first_column_text">Databases</div>
+              <NavLink to="" className="first_column_text">
+                <div
+                  key={4}
+                  className={active === "4" ? "active" : undefined}
+                  id={"4"}
+                  onClick={handleClick}
+                >
+                  Databases
+                </div>
               </NavLink>
-              <NavLink to="" activeClassName="active-link">
-                <div className="first_column_text">Blockchain Technology</div>
+              <NavLink to="" className="first_column_text">
+                <div
+                  key={5}
+                  className={active === "5" ? "active" : undefined}
+                  id={"5"}
+                  onClick={handleClick}
+                >
+                  Blockchain Technology
+                </div>
               </NavLink>
-              <NavLink to="" activeClassName="active-link">
-                <div className="first_column_text">Cloud Services</div>
+              <NavLink to="" className="first_column_text">
+                <div
+                  key={6}
+                  className={active === "6" ? "active" : undefined}
+                  id={"6"}
+                  onClick={handleClick}
+                >
+                  Cloud Services
+                </div>
               </NavLink>
             </div>
             <div className="tool_icons_column">
@@ -133,22 +180,23 @@ const Wrapper = styled.section`
     grid-template-columns: 1fr 0.5fr 0.5fr 0.5fr 0.5fr;
     display: flex;
     align-items: center;
-    justify-content:center;
+    justify-content: center;
     gap: 8rem;
     .first_column {
       width: 25rem;
       height: 30rem;
       background: ${({ theme }) => theme.colors.color_grey};
       .first_column_text {
-        padding: 1.4rem;
+        margin: 1rem;
         font-size: 1.6rem;
         color: ${({ theme }) => theme.colors.heading};
-        &:hover,
-        .active {
-          background: ${({ theme }) => theme.colors.component_bg};
-          color: ${({ theme }) => theme.colors.color_white};
-        }
       }
+    }
+
+    .active {
+      background: ${({ theme }) => theme.colors.component_bg};
+      color: ${({ theme }) => theme.colors.color_white};
+      padding: 1rem 0;
     }
     .tool_icons_column {
       display: flex;
