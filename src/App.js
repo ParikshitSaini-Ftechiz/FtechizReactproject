2ca79ec1d3ba4a5b2e37dbfb196612d49344901f
KajalKushwahaDeveloper <kajalkushwaha231096@gmail.com>
2023-02-10 17:28:01 +0530

    Added images in toot tech component

--- src/App.js
+++ src/App.js
@@ -86,6 +86,7 @@ function App() {
           <Header />
           <Scroll_Up />
           <Routes>
+            <Route path="/" element={<Home />} />
             <Route path="/home" element={<Home />} />
             <Route path="/about" element={<About />} />
 
